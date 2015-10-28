8ce6da53d8caf23a0dc28f6057bfc2402697c93c
hisasann <hisasann25@gmail.com>
2015-10-28 16:50:54 +0900

    Initial Commit

---
+++ izakaya-projectTests/izakaya_projectTests.swift
@@ -0,0 +1,36 @@
+//
+//  izakaya_projectTests.swift
+//  izakaya-projectTests
+//
+//  Created by 久松 由幸 on 2015/10/28.
+//  Copyright © 2015年 hisasann. All rights reserved.
+//
+
+import XCTest
+@testable import izakaya_project
+
+class izakaya_projectTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
